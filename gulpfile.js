3ef3690918a4a2a26b0068cd0c82a6f5019ae695
patelravi <ravipatel.ce@gmail.com>
2019-01-25 09:58:29 +0530

    Added list and retreive address apis

--- gulpfile.js
+++ gulpfile.js
@@ -48,7 +48,7 @@ gulp.task('pre-test', function () {
 gulp.task('test', ['pre-test'], function (cb) {
   var mochaErr;
 
-  gulp.src('test/**/*.js')
+  gulp.src('test/**/verification.js')
     .pipe(plumber())
     .pipe(mocha({ reporter: 'spec' }))
     .on('error', function (err) {
--- test/verification.js
+++ test/verification.js
@@ -13,7 +13,9 @@ describe('phlo client init', function () {
 
     console.log("ABCD");
 
-    client.addressVerification.get(5);
+    // client.verification.retreive_address(5);
+    // await client.verification.list_all_addresses();
+    await client.verification.create_address('IN', 'abc', 'mr', 'shweta', 'ravi', 'bhagwati', 'sector', 'mumbai', 'maharashtra', '410209', 'iso', 'gdusgfuauf', 'alias', 'file', 'aadhar', '123', 'fascal123', 'st12', 'mu34');
 
   });
 
