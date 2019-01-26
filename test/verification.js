623d79172e34596588c159ee021518d87ddffca7
patelravi <ravipatel.ce@gmail.com>
2019-01-26 12:12:01 +0530

    Modified auth token.

--- test/verification.js
+++ test/verification.js
@@ -2,8 +2,11 @@ import {
   Client
 } from '../lib/rest/client';
 
-let authId = 'MAZJJKMWNLZJNIYJKYYT';
-let authToken = 'ZTQyYjI5NjkyMWE2N2YzMmM3ZWZiYWQ1YWI1NzAw';
+// let authId = 'MAZJJKMWNLZJNIYJKYYT';
+// let authToken = 'ZTQyYjI5NjkyMWE2N2YzMmM3ZWZiYWQ1YWI1NzAw';
+
+let authId = 'MAMTI0ZWVIMDC5MMRIOT';
+let authToken = 'ZTRmNWUyMWJlYWMyNDc2NjliNzViODdmYjM1ZWNk';
 
 let client = new Client(authId, authToken);
 
@@ -14,8 +17,8 @@ describe('phlo client init', function () {
     console.log("ABCD");
 
     // client.verification.retreive_address(5);
-    // await client.verification.list_all_addresses();
-    await client.verification.create_address('IN', 'abc', 'mr', 'shweta', 'ravi', 'bhagwati', 'sector', 'mumbai', 'maharashtra', '410209', 'iso', 'gdusgfuauf', 'alias', 'file', 'aadhar', '123', 'fascal123', 'st12', 'mu34');
+    await client.verification.list_all_addresses();
+    // await client.verification.create_address('IN', 'abc', 'mr', 'shweta', 'ravi', 'bhagwati', 'sector', 'mumbai', 'maharashtra', '410209', 'iso', 'gdusgfuauf', 'alias', 'file', 'aadhar', '123', 'fascal123', 'st12', 'mu34');
 
   });
 
