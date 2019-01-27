b655adbc0ba5195a0696a7befa3299e9dd038c2a
patelravi <ravipatel.ce@gmail.com>
2019-01-27 23:24:38 +0530

    Added identify verification methods.

--- test/test.verification.js
+++ test/test.verification.js
@@ -16,10 +16,14 @@ describe('phlo client init', function () {
 
     console.log("ABCD");
 
-    // client.verification.retreive_address(5);
-    await client.verification.list_all_addresses();
-    // await client.verification.create_address('IN', 'abc', 'mr', 'shweta', 'ravi', 'bhagwati', 'sector', 'mumbai', 'maharashtra', '410209', 'iso', 'gdusgfuauf', 'alias', 'file', 'aadhar', '123', 'fascal123', 'st12', 'mu34');
-
+    // await client.verification.retreive_address(5);
+    //  await client.verification.list_all_addresses();
+    //  await client.verification.create_address('IN', 'abc', 'mr', 'shweta', 'ravi', 'bhagwati', 'sector', 'mumbai', 'maharashtra', '410209', 'iso', 'gdusgfuauf', 'alias', 'file', 'aadhar', '123', 'fascal123', 'st12', 'mu34');
+   
+    // client.verification.retreive_identity(5);
+    // await client.verification.list_all_identity();
+    // await client.verification.create_identity('IN', 'abc', 'mr', 'shweta', 'ravi', 'bhagwati', 'sector', 'mumbai', 'maharashtra', '410209', 'iso', 'gdusgfuauf', 'alias', 'file', 'aadhar', '123', 'indian','ind-123','gujarat','01-02-1991','id-date-12/10/2017','bus-plivo','fascal123', 'st12', 'mu34');
+    client.verification.delete_identity(5); 
   });
 
 });
